789cc8707d409cf5f8338e7fedcc1164f799b75e
MoonstruckOccultist <justgive1234@gmail.com>
2024-04-10 14:06:50 -0600

    updated path

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const { expressMiddleware } = require('@apollo/server/express4');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
-const { typeDefs, resolvers } = require('./schemas')
+const { typeDefs, resolvers } = require('./Schemas')
 const { authMiddleware } = require('./utils/auth')
 
 const PORT = process.env.PORT || 3001;
